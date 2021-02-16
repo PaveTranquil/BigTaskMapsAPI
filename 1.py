9ff8e2b44ddf9df4ff79cd870c9abe54a770a7f8
Jabca <pavlo.sterlon@gmail.com>
2021-02-16 17:45:08 +0300

    Масштаb

--- 1.py
+++ 1.py
@@ -1,8 +1,10 @@
-import pygame
 import sys
-import os
+
+import pygame
+import pygame.display
+import pygame.image
+import pygame.transform
 import requests
-import pygame.image, pygame.display, pygame.transform
 
 API_KEY = '40d1649f-0493-4b70-98ba-98533de7710b'
 size = (600, 450)
@@ -92,20 +94,21 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-
+                exit()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_DOWN:
-                    spn[0] -= 1
-                elif event.key == pygame.K_UP:
-                    spn[1] += 1
-                elif event.key == pygame.K_RIGHT:
-                    spn[0] -= 1
-                elif event.key == pygame.K_LEFT:
-                    spn[1] += 1
+                coords = [float(i) for i in spn[0].split(",")]
+                coords = [str(i) for i in spn[0].split(",")]
+                spn = (coords[0] + "," + coords[1], spn[1])
+                spn_list = list(map(float, spn[1].split(',')))
+                if event.key == pygame.K_PAGEUP:
+                    spn_list[0] = spn_list[0] * 2
+                    spn_list[1] = spn_list[1] * 2
+                elif event.key == pygame.K_PAGEDOWN:
+                    spn_list[0] = spn_list[0] / 2
+                    spn_list[1] = spn_list[1] / 2
+                spn = [spn[0], ','.join(list(map(str, spn_list)))]
                 ll_spn = f"ll={spn[0]}&spn={spn[1]}"
                 show_map(ll_spn, "map")
-                print("ADSAS")
-    os.remove(map_file)
 
 
 if __name__ == "__main__":
