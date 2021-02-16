cdb0659db235282d57df4d9dcc26cfaecb8e4e93
Jabca <pavlo.sterlon@gmail.com>
2021-02-16 17:52:54 +0300

    Теперь и мсаштаб и стрелочки

--- 1.py
+++ 1.py
@@ -1,7 +1,8 @@
-import pygame
 import sys
-import requests
+
+import pygame
 import pygame.display
+import requests
 
 pygame.init()
 API_KEY = '40d1649f-0493-4b70-98ba-98533de7710b'
@@ -95,6 +96,10 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
+            coords = [float(i) for i in spn[0].split(",")]
+            print(str(coords[0]))
+            spn = (str(coords[0]) + "," + str(coords[1]), spn[1])
+            spn_list = list(map(float, spn[1].split(',')))
             if event.type == pygame.KEYDOWN:
                 coords = [float(i) for i in coords]
                 if event.key == pygame.K_DOWN:
@@ -109,6 +114,13 @@ def main():
                     vid += 1
                     vid = vid % 3
                 coords = [str(i) for i in coords]
+                if event.key == pygame.K_PAGEUP:
+                    spn_list[0] = spn_list[0] * 2
+                    spn_list[1] = spn_list[1] * 2
+                elif event.key == pygame.K_PAGEDOWN:
+                    spn_list[0] = spn_list[0] / 2
+                    spn_list[1] = spn_list[1] / 2
+                spn = [spn[0], ','.join(list(map(str, spn_list)))]
                 ll_spn = f"ll={coords[0]},{coords[1]}&spn={spn[1]}"
                 if vid == 0:
                     show_map(ll_spn, "map")
